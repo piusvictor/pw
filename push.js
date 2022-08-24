7a6c912d2b7edcb6055cf2391bde5a619733427c
Pius Victor <v.pius.pv@gmail.com>
2022-08-24 13:56:19 +0300

    Updating service worker file

--- push.js
+++ push.js
@@ -1,6 +1,11 @@
 const push=require('web-push');
 //let pubprvkeys= push.generateVAPIDKeys();
-const pubprvkeys={
+const validKeys={
     publicKey: 'BCqpCNT1hZpZVPQ7ht0KxNgTfS43-jzrkjfHdPJLQi_QPu0Wmqwtl4q97eC92Ut20egOFg0BVVDE9b-ojd3xKEE',
     privateKey: '2f9nkhuILBAULTPDrs171U8KnTqd8wxHTkt0orRd4uQ'
   }
+
+  push.setVapidDetails('mailto:v.pius.pv@gmail.com',validKeys.publicKey,validKeys.privateKey);
+  let sub={}
+
+  push.sendNotification(sub,'text message')
