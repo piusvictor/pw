0bf269f77ec42b5e728792507d30285bca219c4b
Pius Victor <v.pius.pv@gmail.com>
2022-08-24 11:57:49 +0300

    first commit

---
+++ push.js
@@ -0,0 +1,6 @@
+const push=require('web-push');
+//let pubprvkeys= push.generateVAPIDKeys();
+const pubprvkeys={
+    publicKey: 'BCqpCNT1hZpZVPQ7ht0KxNgTfS43-jzrkjfHdPJLQi_QPu0Wmqwtl4q97eC92Ut20egOFg0BVVDE9b-ojd3xKEE',
+    privateKey: '2f9nkhuILBAULTPDrs171U8KnTqd8wxHTkt0orRd4uQ'
+  }
